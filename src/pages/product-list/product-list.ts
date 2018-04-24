2776ee77d0466f70f8474a47b0895b4c4e5dc9fd
Unknown <davidrg095@gmail.com>
2018-04-24 13:47:29 +0100

    Product List + Firebase read + Refresher

--- src/pages/product-list/product-list.ts
+++ src/pages/product-list/product-list.ts
@@ -2,13 +2,32 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {AddProductPage} from "../add-product/add-product";
 
+import {ProductListService} from "../../services/product-list/product-list.service";
+import {ProductItem} from "../../models/product-item/product-item.model";
+import {Observable} from "rxjs/Observable";
+
 @Component({
   selector: 'page-product-list',
   templateUrl: 'product-list.html',
 })
 export class ProductListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  productList$: Observable<ProductItem []>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private list: ProductListService) {
+
+    this.productList$ = this.list
+      .getProductList()    // Return DB list
+      .snapshotChanges()   // Obtain values and key - valueChanges() for only values
+      .map(
+        changes => {
+          return changes.map(c => ({    //Return new object for every change
+            key: c.payload.key,
+            ...c.payload.val()
+          })).reverse()
+        }
+      )
+
   }
 
   navToAddProductPage() {
@@ -16,4 +35,13 @@ export class ProductListPage {
     this.navCtrl.parent.select(0);
     this.navCtrl.popToRoot();
   }
+
+  doRefresh(refresher) {
+
+    setTimeout(() => {
+      refresher.complete();
+    }, 2000);
+
+  }
+
 }
