d39e025fb0f9a39a33756cf9e1a6382fa4fe8a16
Kaai <kaai.cyre@gmail.com>
2018-04-24 23:47:50 +0100

    Added Firebase login and signup
    TODO logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+
+import { AuthService} from "../services/auth.service";
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = LoginPage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+              private auth: AuthService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  login() {
+    this.auth.signOut();
+    this.nav.setRoot(LoginPage);
+  }
+
+  logout() {
+    this.auth.signOut();
+    this.nav.setRoot(TabsPage);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,26 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { NgxErrorsModule } from '@ultimate/ngxerrors';
 
 import {ProductListPage} from "../pages/product-list/product-list";
 import {AddProductPage} from "../pages/add-product/add-product";
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+import {MyProductsPage} from "../pages/my-products/my-products";
+import { SignupPage} from "../pages/signup/signup";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { AngularFireModule } from "angularfire2";
 import { AngularFireDatabaseModule } from "angularfire2/database";
+import {AngularFireAuth, AngularFireAuthModule} from "angularfire2/auth"
+import { AngularFireModule } from "angularfire2";
 
-import { FIREBASE_CONFIG } from "./firebase.credentials";
-import {MyProductsPage} from "../pages/my-products/my-products";
+
 import {ProductListService} from "../services/product-list/product-list.service";
+import { AuthService} from "../services/auth.service";
+import { FIREBASE_CONFIG } from "../config/firebase.credentials";
 
 
 
@@ -25,13 +31,18 @@ import {ProductListService} from "../services/product-list/product-list.service"
     TabsPage,
     AddProductPage,
     ProductListPage,
-    MyProductsPage
+    MyProductsPage,
+    LoginPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(FIREBASE_CONFIG),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    NgxErrorsModule
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -39,13 +50,17 @@ import {ProductListService} from "../services/product-list/product-list.service"
     TabsPage,
     AddProductPage,
     ProductListPage,
-    MyProductsPage
+    MyProductsPage,
+    LoginPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    ProductListService
+    ProductListService,
+    AuthService,
+    AngularFireAuth
   ]
 })
 export class AppModule {}
