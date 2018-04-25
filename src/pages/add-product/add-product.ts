e1d0c0fa878f86dcdd83839d62e5c00fddda1a53
Unknown <davidrg095@gmail.com>
2018-04-25 18:08:13 +0100

    Camera plugin working

--- src/pages/add-product/add-product.ts
+++ src/pages/add-product/add-product.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {AngularFireDatabase} from "angularfire2/database";
-import {ProductItem} from "../../models/product-item/product-item.model";
+import { ProductItem } from "../../models/product-item/product-item.model";
+import { Camera, CameraOptions} from "ionic-native/";
 
 import {MyProductsPage} from "../my-products/my-products";
-
 
 import {ProductListService} from "../../services/product-list/product-list.service";
 
@@ -18,8 +18,8 @@ export class AddProductPage {
     name: '',
     description: '',
     price: undefined,
+    image: '',
   };
-
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase, private list: ProductListService) {
   }
@@ -27,9 +27,26 @@ export class AddProductPage {
   addProduct(product: ProductItem) {
     this.list.addProduct(product).then(ref => {
       console.log(ref.key); //Get key and show it on console
-      this.navCtrl.setRoot(MyProductsPage, {key: ref.key})  //Goes to MyProductsPage with key
+      this.navCtrl.push(MyProductsPage, {key: ref.key});  //Goes to MyProductsPage with key
       this.navCtrl.parent.select(2);
       this.navCtrl.popToRoot();
+    })
+  }
+
+  takePhoto() {
+    let options: CameraOptions = {
+      quality: 100,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: false
+    };
+
+    Camera.getPicture(options).then((imageData) => {
+      this.product.image = `data:image/jpeg;base64,${imageData}`;
     })
   }
 
